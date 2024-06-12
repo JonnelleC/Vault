896036eee33253ce53c3fb417ba030317c260c16
JonnelleC <163946264+JonnelleC@users.noreply.github.com>
2024-06-12 01:44:47 -0400

    Add files via upload

---
+++ vault.js
@@ -0,0 +1,11 @@
+/// Display pop up message to user
+window.alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+//Create 3 calculations to produce required combination 
+
+let firstNumber = 5 * 2 ;
+let secondNumber = 120 /3 ;
+let thirdNumber = 13 + 26 ;
+
+// display combination 
+console.log(firstNumber, secondNumber, thirdNumber);
